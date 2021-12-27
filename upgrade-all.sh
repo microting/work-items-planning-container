b2f56400796c48fb81211c6cc8f4f67b7dc654a9
René Schultz Madsen <rm@microting.com>
2021-12-27 07:25:30 +0000

    Lowring the sleep time to 10 seconds for upgrade-all script.

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -6,5 +6,5 @@ for CUSTOMER in ${CUSTOMERS[@]}; do
   echo "Updating customer with no $CUSTOMER"
   ./upgrade.sh $CUSTOMER
   echo "Done updating customer with no $CUSTOMER, sleeping 60 seconds before proceeding to the next"
-  sleep 90
+  sleep 10
 done
