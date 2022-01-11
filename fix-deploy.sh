bbc0c1fcbc0f7cc29bce1087955c852b80408ba7
René Schultz Madsen <rm@microting.com>
2022-01-11 16:25:58 +0100

    Updating the fix-deploy to fix broken AreaRulesId to AreaRuleId in the backend plugin db.

--- fix-deploy.sh
+++ fix-deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
-CUSTOMERS=('238' '419' '715' '734' '743' '750' '754' '756' '769' '777' '780' '783' '784' '788' '790' '791' '798' '800' '802' '805' '806' '811' '812' '815' '817' '818' '819' '820' '823' '824' '826' '827' '828' '829' '830' '834' '835' '836' '837' '839' '840' '841' '842' '843' '844' '845' '846' '847' '848' '849' '850' '851' '852' '853' '854' '855' '856' '857' '858' '859' '860' '861' '862' '863' '864' '865' '866' '867' '868' '869' '870' '871' '872' '873')
+CUSTOMERS=('238' '419' '715' '734' '743' '750' '754' '756' '769' '777' '780' '783' '784' '798' '800' '802' '815' '818' '819' '834' '836' '837' '840' '844' '845' '846' '847' '848' '849' '850' '851' '852' '853' '854' '855' '856' '857' '858' '859' '860' '861' '862' '863' '864' '865' '866' '867' '868' '869' '870' '871' '872' '873' '874' '875' '876' '877' '878' '879' '880' '881' '882' '884' '885' '886' '887' '888' '889' '890' '891' '892')
 
 for CUSTOMER in ${CUSTOMERS[@]}; do
   echo "Updating customer with no $CUSTOMER"
 
   HOST="frontend-$CUSTOMER-mariadb-0"
-  DATABASE_NAME="$CUSTOMER""_eform-angular-items-planning-plugin"
+  DATABASE_NAME="$CUSTOMER""_eform-backend-configuration-plugin"
 
-  kubectl -n customers exec -it $HOST -- mysql -u root -psecretpassword -e 'use `'$DATABASE_NAME'`; update PluginConfigurationValues set Value = 7 where Name = "ItemsPlanningBaseSettings:StartTime"'
-  kubectl -n customers exec -it $HOST -- mysql -u root -psecretpassword -e 'use `'$DATABASE_NAME'`; update PluginConfigurationValues set Value = 9 where Name = "ItemsPlanningBaseSettings:EndTime"'
+  kubectl -n customers exec -it $HOST -- mysql -u root -psecretpassword -e 'ALTER TABLE `'$DATABASE_NAME'`.`AreaRulesPlanningVersions` CHANGE COLUMN `AreaRulesId` `AreaRuleId` INT(11) NOT NULL ;'
+
+  #kubectl -n customers exec -it $HOST -- mysql -u root -psecretpassword -e 'use `'$DATABASE_NAME'`; update PluginConfigurationValues set Value = 7 where Name = "ItemsPlanningBaseSettings:StartTime"'
+  #kubectl -n customers exec -it $HOST -- mysql -u root -psecretpassword -e 'use `'$DATABASE_NAME'`; update PluginConfigurationValues set Value = 9 where Name = "ItemsPlanningBaseSettings:EndTime"'
 
   echo "Done updating customer with no $CUSTOMER"
 done
+
+
+# ALTER TABLE `AreaRulesPlanningVersions` CHANGE COLUMN `AreaRulesId` `AreaRuleId` INT(11) NOT NULL ;
