54d5c7e18ddb6315c7299cd8a4f0f0870d3d2deb
René Schultz Madsen <rm@microting.com>
2022-06-24 04:43:05 +0000

    Bumping due to updates in backend.

--- fix-islocked.sh
+++ fix-islocked.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUSTOMERS=('914')
+CUSTOMERS=('915')
 
 for CUSTOMER in ${CUSTOMERS[@]}; do
   echo "Updating customer with no $CUSTOMER"
@@ -7,7 +7,7 @@ for CUSTOMER in ${CUSTOMERS[@]}; do
   HOST="frontend-$CUSTOMER-mariadb-0"
   DATABASE_NAME="$CUSTOMER""_SDK"
 
-  kubectl -n customers exec -it $HOST -- mysql -u root -psecretpassword -e 'use `'$DATABASE_NAME'`; update CheckLists set IsLocked = 0 where Id IN (63,7)'
+  kubectl -n customers exec -it $HOST -- mysql -u root -psecretpassword -e 'use `'$DATABASE_NAME'`; update CheckLists set IsLocked = 1 where Id IN (3)'
 
   echo "Done updating customer with no $CUSTOMER"
 done
