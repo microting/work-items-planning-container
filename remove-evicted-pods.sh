cad9c3be5421d72cef4f9b7c191a05ccf80bb109
René Schultz Madsen <rm@microting.com>
2022-07-07 18:23:49 +0000

    Bumping due to changes in SDK.

--- remove-evicted-pods.sh
+++ remove-evicted-pods.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-kubectl get pods -n customers | grep Evicted | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
+kubectl get pods -n customers | grep Error | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
+kubectl get pods -n customers | grep ContainerStatusUnknown | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
