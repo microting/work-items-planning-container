054767c40587852e9edacb461f357536feda30aa
René Schultz Madsen <rm@microting.com>
2022-08-25 09:41:14 +0000

    Bumping due to updates in plugins.

--- remove-evicted-pods.sh
+++ remove-evicted-pods.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 kubectl get pods -n customers | grep Error | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
 kubectl get pods -n customers | grep ContainerStatusUnknown | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
+kubectl get pods -n customers | grep Completed | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
