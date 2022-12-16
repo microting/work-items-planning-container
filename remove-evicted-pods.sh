89551a91f4f33ea4558cfb093823872585b5754e
René Schultz Madsen <rm@microting.com>
2022-12-16 12:57:39 +0100

    Changing the code to publish to public docker hub.

--- remove-evicted-pods.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kubectl get pods -n customers | grep Error | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
-kubectl get pods -n customers | grep ContainerStatusUnknown | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
-kubectl get pods -n customers | grep Completed | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
-kubectl get pods -n customers | grep Evicted | cut -d " " -f 1 | xargs kubectl delete pods -n customers --grace-period=0 --force
